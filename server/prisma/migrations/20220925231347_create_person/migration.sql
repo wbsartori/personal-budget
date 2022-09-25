d9de2af5a764859b0e76d814f1be85f2c2338fde
wbsartori <wbsartori@gmail.com>
2022-09-25 20:14:22 -0300

    projeto inicial

---
+++ server/prisma/migrations/20220925231347_create_person/migration.sql
@@ -0,0 +1,8 @@
+-- CreateTable
+CREATE TABLE "person" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "birthDate" TEXT NOT NULL,
+    "gender" TEXT NOT NULL,
+    "email" TEXT NOT NULL
+);
