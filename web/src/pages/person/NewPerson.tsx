9290c1d8b4018c4e9ba7b31d64627e27390c1c9d
wbsartori <wbsartori@gmail.com>
2022-10-13 23:26:48 -0300

    create person

--- web/src/pages/person/NewPerson.tsx
+++ web/src/pages/person/NewPerson.tsx
@@ -1,13 +1,7 @@
-import {Input} from "../../components/Form/Input";
-
-import * as Dialog from "@radix-ui/react-dialog";
-import * as Checkbox from "@radix-ui/react-checkbox";
-import * as ToggleGroup from "@radix-ui/react-toggle-group";
-
-
 import {FormEvent, useEffect, useState} from "react";
 import axios from "axios";
-import {Button} from "../../components/Form/Button";
+import { useNavigate } from "react-router-dom";
+import {Input} from "../../components/Form/Input";
 
 interface IPerson {
     id: string;
@@ -16,16 +10,7 @@ interface IPerson {
 
 export function NewPerson() {
     const [persons, setPersons] = useState<IPerson[]>([]);
-    const [weekDays, setWeekDays] = useState<string[]>([]);
-    const [useVoiceChannel, setUseVoiceChannel] = useState(false);
-
-    // useEffect(() => {
-    //     axios('http://localhost:3434/person')
-    //         .then(response => {
-    //             console.log(response.data)
-    //             setPersons(response.data);
-    //         });
-    // });
+    const errors = [];
 
     async function handleCreatePerson(event: FormEvent) {
         event.preventDefault();
@@ -33,39 +18,45 @@ export function NewPerson() {
         const formData = new FormData(event.target as HTMLFormElement);
         const data = Object.fromEntries(formData);
 
-        //validação
-
-        if (!data.data) {
-            return;
+        if (data.name === '') {
+            errors.push({'errors' : 'nome'});
+        }
+        if (data.birthDate === '') {
+            errors.push({'errors' : 'birthDate'});
+        }
+        if (data.gender === '') {
+            errors.push({'errors' : 'gender'});
+        }
+        if (data.email === '') {
+            errors.push({'errors' : 'email'});
         }
 
         try {
+
             await axios.post(`http://localhost:3434/person`, {
                 name: data.name,
-                birthDate: Number(data.birthDate),
+                birthDate: data.birthDate,
                 gender: data.gender,
                 email: data.email,
                 status: data.status,
             });
+
             alert('Pessoa criada com sucesso!');
         } catch (err) {
             console.log(err);
             alert('Erro ao criar pessoa!');
         }
+
+        // navigate("/person");
     }
 
     return (
         <>
             <h1 className="mt-5 text-white mb-5">Pessoas \ Novo</h1>
             <hr/>
-            <form  className="mt-8 flex flex-col gap-4">
+            <form  onSubmit={handleCreatePerson} className="mt-8 flex flex-col gap-4">
                 <div className="w-full mt-5 mb-5">
-                    <a className="h-10 w-1/12 bg-green-600 rounded-md"
-                       href="/person">
-                        <button type="submit" className="h-10 w-1/12 bg-green-600 rounded-md">
-                            Salvar
-                        </button>
-                    </a>
+                    <button type="submit" className="h-10 w-1/12 bg-green-600 rounded-md">Salvar</button>
                     <a className="h-10 w-1/12 bg-green-600 rounded-md ml-2"
                        href="/person">
                         <button type="button" className="h-10 w-1/12 bg-red-600 rounded-md">
@@ -101,6 +92,19 @@ export function NewPerson() {
                 <div className="flex flex-col gap-2 w-3/6">
                     <label htmlFor="email" className="text-white">E-mail </label>
                     <Input type="email" id="email" name="email" placeholder="E-mail"/>
+                </div>
+                <div className="flex flex-col gap-2 w-3/12">
+                    <label htmlFor="status" className="text-white">Status </label>
+                    <select name="status" id="status" className="w-full
+                                  rounded-md
+                                  focus:border-indigo-500
+                                  focus:ring-indigo-500
+                                  h-10
+                                  mb-5">
+                        <option value=""/>
+                        <option value="A">Ativo</option>
+                        <option value="I">Inativo</option>
+                    </select>
                 </div>
             </form>
         </>
